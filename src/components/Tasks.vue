8866b4bb75f9a54a18c6c3f17f6abb3e42f87c97
kosa4real <kosa4real0001@gmail.com>
2021-09-07 12:51:49 +0100

    task-tracker first commit

---
+++ src/components/Tasks.vue
@@ -0,0 +1,45 @@
+<template>
+  <div 
+    v-for="task in tasks"
+    :key='task.id' >
+    <Task 
+      @delete-task="$emit('delete-task', task.id)" 
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      :task="task" />
+  </div>
+</template>
+
+<script>
+  import Task from './Task.vue'
+
+  export default{
+    name: 'Tasks',
+    props:{
+      tasks: Array,
+    },
+    components:{
+      Task,
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+  }
+</script>
+
+<style scope>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
